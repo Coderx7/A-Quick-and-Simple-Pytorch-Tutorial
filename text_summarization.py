3d82772e750b8be4152e7416220ff209e42c5a0f
coderx7 <coderx7@gmail.com>
2024-01-19 22:23:42 +0330

    added link for text-summarization

--- text_summarization.py
+++ text_summarization.py
@@ -1,3 +1,4 @@
 #%% in the name of God the most compassionate the most merciful
 # in t his section we will be implementing a text-summarization
-# demo
+# demo :‌https://www.youtube.com/watch?v=CDmPBsZ09wg 
+# https://www.youtube.com/watch?v=qaPMdcCqtWk
