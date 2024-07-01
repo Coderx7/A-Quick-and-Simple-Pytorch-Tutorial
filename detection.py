fb2973f926b63fc1137d94e5a5eeaffd0689b1fa
coderx7 <coderx7@gmail.com>
2024-07-01 21:04:28 +0330

    reminder for working on detection using owlv1 and owlv2 (Vision Transformer for Open-World Localization)

---
+++ detection.py
@@ -0,0 +1,7 @@
+# in the name of God the most compassionate the most merciful 
+# https://huggingface.co/docs/transformers/en/model_doc/owlv2
+# https://github.com/huggingface/notebooks/blob/main/examples/zeroshot_object_detection_with_owlvit.ipynb
+# https://huggingface.co/docs/transformers/en/model_doc/owlvit
+# https://medium.com/@Mert.A/zero-shot-object-detection-mit-owl-vit-und-huggingface-86c7e568c18a
+
+
