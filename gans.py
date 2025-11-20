6dea2b28a3445992390f64ca8ba223149bd107d6
Seyyed Hossein Hasanpour <coderx7@gmail.com>
2025-11-20 20:21:16 +0330

    add make_gif so we get better visualization and see gradual changes

--- gans.py
+++ gans.py
@@ -10776,7 +10776,7 @@ w_avg = get_w_avg(sample_size=sample_size)
 
 # now to get a better overall view of noise_injection
 # lets keep everything the same except constant_noise(disable noise injection)
-def run_interpolation_test(step, w_avg_sample_size, constant_noise, psi_rates=None, num_samples=36):
+def run_interpolation_test(step, w_avg_sample_size, constant_noise, psi_rates=None, alphas=None, num_samples=36, make_gifs=False, interval=300):
     
     if psi_rates is None:
         psi_rates = [0, 0.3, 0.7, 1]
@@ -10792,19 +10792,32 @@ def run_interpolation_test(step, w_avg_sample_size, constant_noise, psi_rates=No
     print(f'self.ema_w norm: {generator_copy.ema_w.norm().item()}')
     print(f'manual w_avg norm: {w_avg.norm().item()}')
     
-    interps0 = interpolate_w(generator_copy, z1, z2, step, psi=None, w_avg=None, constant_noise=constant_noise, interp_steps=num_samples)
+    interps0 = interpolate_w(generator_copy, z1, z2, step, psi=None, w_avg=None, 
+                             constant_noise=constant_noise, alphas=alphas, interp_steps=num_samples)
     display_images(interps0, title=f'psi=None | constant_noise:{constant_noise}', cols=cols, unnormalize=True)
     
     for rate in psi_rates:
-        interps1 = interpolate_w(generator_copy, z1, z2, step, psi=rate, w_avg=None, constant_noise=constant_noise, interp_steps=num_samples)
-        interps2 = interpolate_w(generator_copy, z1, z2, step, psi=rate, w_avg=w_avg, constant_noise=constant_noise, interp_steps=num_samples)
-    
+        interps1 = interpolate_w(generator_copy, z1, z2, step, psi=rate, w_avg=None,
+                                 constant_noise=constant_noise, alphas=alphas, interp_steps=num_samples)
+        interps2 = interpolate_w(generator_copy, z1, z2, step, psi=rate, w_avg=w_avg, 
+                                 constant_noise=constant_noise, alphas=alphas, interp_steps=num_samples)
+        
         display_images(interps1, title=f'psi={rate} + ema_w | constant_noise:{constant_noise}', cols=cols, unnormalize=True)
         display_images(interps2, title=f'psi={rate} + w-avg(sz={w_avg_sample_size:,}) | constant_noise:{constant_noise}', cols=cols, unnormalize=True)
-
+        
+        # lets also make some gifs    
+        if make_gifs:
+            fname =f"interp_psi_None_const_noise_{constant_noise}"
+            create_interpolation_animation(interps0, fname, interval=interval)
+            
+            fname =f"interp_psi_{rate}_ema_w_const_noise_{constant_noise}"
+            create_interpolation_animation(interps1, fname, interval=interval)
+            
+            fname = f"interp_psi_{rate}_w_avg(sz_{w_avg_sample_size})_const_noise_{constant_noise}"
+            create_interpolation_animation(interps2, fname, interval=interval)
 
 # now lets try 
-num_samples = 64
+num_samples = 36
 cols = int(num_samples**0.5)
 
 z1 = torch.randn(size=(1, generator_style1.z_size))
@@ -10827,8 +10840,15 @@ run_interpolation_test(last_step,
                        #psi_rates=[0,0.5,1.2,3,5],
                        constant_noise=False,
                        num_samples=num_samples)
-
-
+#%%
+# now lets also make some gifs as well!
+for noise_status in [True, False]:
+    run_interpolation_test(last_step, sample_size,
+                           psi_rates=[0, 0.7],
+                           constant_noise=noise_status,
+                           num_samples=60,
+                           make_gifs=True,
+                           interval=100)
 
 #%%
 # debug logs:
